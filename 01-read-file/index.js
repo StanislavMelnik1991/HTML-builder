56807a25464ee33510cf61c287d9ebc5e494d7b9
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-05-18 14:00:22 +0300

    feat: complete 01-read-file

---
+++ 01-read-file/index.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+const stream = new fs.ReadStream(path.join(__dirname, 'text.txt'), {encoding: 'utf-8'});
+
+stream.on('readable', function(){
+  const data = stream.read();
+  if (data !== null) {
+    console.log(data);
+  }
+});
+ 
