13c668317354f8a6a5ff55ed435ca02b63a52d7f
Stanislav Melnik <stanislavmelnik@gmail.com>
2022-05-21 14:55:46 +0300

    feat: comlepe 04-copy-directory

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,23 @@
+const fs = require('fs');
+const path = require('path');
 
+async function copyDirContain(src, dest){
+  fs.promises.readdir(src, {withFileTypes: true})
+    .then((filenames) => {
+      fs.mkdir(dest,{recursive: true}, err => {
+        if (err) throw err;
+      });
+      for (let file of filenames) {
+        if (file.isFile()) {
+          fs.copyFile(src+'\\'+file.name, dest+'\\'+file.name, err=>{if (err) throw err;});
+        }
+        else{
+          copyDirContain(src+'\\'+file.name, dest+'\\'+file.name);
+        }
+      }
+    })
+    .catch(err => {
+      console.log(err);
+    });
+}
+copyDirContain(path.join(__dirname, 'files'),path.join(__dirname, 'files-copy'));
